732e10262b1fcbe8186d9e7c749fbe16732680e1
NexusTheCollector <124541016+NexusTheCollector@users.noreply.github.com>
2023-03-27 20:26:17 -0500

    Add files via upload

--- content/Items/Weapons/FightersSword.cs
+++ content/Items/Weapons/FightersSword.cs
@@ -1,16 +1,16 @@
-using Terraria;
+﻿using Terraria;
 using Terraria.GameContent.Creative;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace TheLegendofTerra.Content.Items.Weapons
+namespace TheLegendofTerra.content.Items.Weapons
 {
     public class FightersSword : ModItem
     {
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Fighter's Sword");
-            Tooltip.SetDefault("A simple sword wielded by most Hylian knights, even the most \nhigh-ranking swordsmen. \nDespite its simplicity, there is much depth to this blade that \nmay come to light should a great hero brandish it... \n\nThis weapon can perform a Spin Attack (except it can't yet).\nSpin attacks with this weapon progressively \ndeal more damage as they pierce enemies.\nDamage resets at the end of the spin.");
+            Tooltip.SetDefault("A simple sword wielded by most knights, even the high-ranking swordsmen.\n Despite its simplicity, there is much depth to this blade that may come to light should a great hero brandish it... \nThis weapon can perform a Spin Attack (except it can't yet).\nSpin attacks with this weapon progressively deal more damage as it pierces enemies.\nDamage resets at the end of the spin.");
             CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
         }
 
