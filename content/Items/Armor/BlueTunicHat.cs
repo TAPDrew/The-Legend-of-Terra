1bc65c3e4e8e7fcf2c536e4229a318620f940442
NexusTheCollector <124541016+NexusTheCollector@users.noreply.github.com>
2023-03-30 22:29:15 -0500

    Add files via upload

---
+++ content/Items/Armor/BlueTunicHat.cs
@@ -0,0 +1,48 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+
+namespace TestMod.Items.Armor
+{
+	// The AutoloadEquip attribute automatically attaches an equip texture to this item.
+	// Providing the EquipType.Head value here will result in TML expecting a X_Head.png file to be placed next to the item's main texture.
+	[AutoloadEquip(EquipType.Head)]
+	public class BlueTunicHat : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			Tooltip.SetDefault("A blue hat made by the Zoras.");
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 18; // Width of the item
+			Item.height = 18; // Height of the item
+			Item.value = Item.sellPrice(gold: 1); // How many coins the item is worth
+			Item.rare = ItemRarityID.Green; // The rarity of the item
+			Item.defense = 8; // The amount of defense the item will give when equipped
+		}
+
+		// IsArmorSet determines what armor pieces are needed for the setbonus to take effect
+		public override bool IsArmorSet(Item head, Item body, Item legs)
+		{
+			return body.type == ModContent.ItemType<BlueTunicShirt>() && legs.type == ModContent.ItemType<HeroesLegs>();
+		}
+
+		// UpdateArmorSet allows you to give set bonuses to the armor.
+		public override void UpdateArmorSet(Player player)
+		{
+			player.setBonus = "Increases dealt damage by 20%"; // This is the setbonus tooltip
+			player.gills = true;
+			player.accFlipper = true;
+		}
+
+		public override void AddRecipes()
+		{
+			CreateRecipe()
+				.AddIngredient(ItemID.DirtBlock)
+				.Register();
+		}
+	}
+}
