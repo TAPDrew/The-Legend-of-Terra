6854b842d6f8d2051c968c17c222e990f6d7ca7f
NexusTheCollector <124541016+NexusTheCollector@users.noreply.github.com>
2023-03-30 20:53:05 -0500

    Add files via upload

---
+++ content/Projectiles/IceBeam.cs
@@ -0,0 +1,22 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.GameContent.Creative;
+
+namespace TestMod.Items.Projectile
+{
+    public class IceBeam : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.CloneDefaults(ProjectileID.FrostBeam);
+            AIType = ProjectileID.FrostBeam;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+        }
+
+        public override string
+          
+            Texture => "Terraria/Images/Projectile_257";
+    }
+}
