4a14421478f46908e9a33502fd52fc90a56c396c
NexusTheCollector <124541016+NexusTheCollector@users.noreply.github.com>
2023-06-10 20:55:52 -0500

    Add files via upload

---
+++ content/Items/Weapons/GanonTowerBat.cs
@@ -0,0 +1,34 @@
+﻿using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+
+namespace TheLegendofTerra.Content.Items.Weapons
+{
+	public class GanonTowerBat : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Magic Hammer");
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 32;
+			Item.height = 32;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.useTime = 20;
+			Item.useAnimation = 20;
+			Item.autoReuse = true;
+			Item.DamageType = DamageClass.Melee;
+			Item.damage = 150;
+			Item.knockBack = 4.5f;
+			Item.value = Item.sellPrice(0, 5, 0, 0);
+			Item.rare = ItemRarityID.LightRed;
+			Item.UseSound = SoundID.Item1;
+		}
+	}
+}
