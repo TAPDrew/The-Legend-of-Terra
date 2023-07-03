fa3ec0dc521c9bec3671f50b20626ae856596f78
NexusTheCollector <124541016+NexusTheCollector@users.noreply.github.com>
2023-07-02 19:05:27 -0500

    Add files via upload

---
+++ content/NPC's/CuccoScout.cs
@@ -0,0 +1,37 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TheLegendofTerra.Content.NPCs
+{
+    public class CuccoScout : ModNPC
+    {
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Cucco Scout");
+
+            Main.npcFrameCount[Type] = Main.npcFrameCount[NPCID.Zombie];
+
+        }
+
+        public override void SetDefaults()
+        {
+            NPC.width = 18;
+            NPC.height = 40;
+            NPC.damage = 14;
+            NPC.defense = 6;
+            NPC.lifeMax = 200;
+            NPC.HitSound = SoundID.NPCHit1;
+            NPC.DeathSound = SoundID.NPCDeath2;
+            NPC.value = 60f;
+            NPC.knockBackResist = 0.5f;
+            NPC.aiStyle = 3; // Fighter AI, important to choose the aiStyle that matches the NPCID that we want to mimic
+
+            AIType = NPCID.Zombie; // Use vanilla zombie's type when executing AI code. (This also means it will try to despawn during daytime)
+            AnimationType = NPCID.Zombie; // Use vanilla zombie's type when executing animation code. Important to also match Main.npcFrameCount[NPC.type] in SetStaticDefaults.
+            Banner = Item.NPCtoBanner(NPCID.Zombie); // Makes this NPC get affected by the normal zombie banner.
+            BannerItem = Item.BannerToItem(Banner); // Makes kills of this NPC go towards dropping the banner it's associated with.
+        }
+    }
+}
