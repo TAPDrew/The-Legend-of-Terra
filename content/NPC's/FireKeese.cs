0a6e65cd1fb2d955de3d7319bf75ed6ccc584e54
NexusTheCollector <124541016+NexusTheCollector@users.noreply.github.com>
2023-08-04 20:38:12 -0500

    Add files via upload

---
+++ content/NPC's/FireKeese.cs
@@ -0,0 +1,51 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.GameContent.Bestiary;
+using Terraria.GameContent.ItemDropRules;
+using Terraria.ModLoader.Utilities;
+using Terraria.DataStructures;
+
+namespace TheLegendofTerra.Content.NPCs
+{
+    public class FireKeese : ModNPC
+    {
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Fire Keese");
+
+            Main.npcFrameCount[Type] = Main.npcFrameCount[NPCID.CaveBat];
+
+        }
+
+        public override void SetDefaults()
+        {
+            NPC.width = 18;
+            NPC.height = 40;
+            NPC.damage = 14;
+            NPC.defense = 6;
+            NPC.lifeMax = 200;
+            NPC.HitSound = SoundID.NPCHit1;
+            NPC.DeathSound = SoundID.NPCDeath2;
+            NPC.value = 60f;
+            NPC.knockBackResist = 0.5f;
+            NPC.aiStyle = 14; // Fighter AI, important to choose the aiStyle that matches the NPCID that we want to mimic
+
+            AIType = NPCID.CaveBat; // Use vanilla zombie's type when executing AI code. (This also means it will try to despawn during daytime)
+            AnimationType = NPCID.CaveBat; // Use vanilla zombie's type when executing animation code. Important to also match Main.npcFrameCount[NPC.type] in SetStaticDefaults.
+            Banner = Item.NPCtoBanner(NPCID.CaveBat); // Makes this NPC get affected by the normal zombie banner.
+            BannerItem = Item.BannerToItem(Banner); // Makes kills of this NPC go towards dropping the banner it's associated with.
+        }
+
+        public override float SpawnChance(NPCSpawnInfo spawnInfo)
+        {
+            return SpawnCondition.OverworldDay.Chance * 0.5f;
+        }
+
+        public override void ModifyNPCLoot(NPCLoot npcLoot)
+        {
+            npcLoot.Add(ItemDropRule.Common(ItemID.Confetti)); // 1% chance to drop Confetti
+        }
+    }
+}
